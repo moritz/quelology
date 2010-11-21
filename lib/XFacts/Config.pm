0dc37898d0283a1a7dd1d2405f965ef809544a37
Moritz Lenz <moritz@faui2k3.org>
2010-11-21 22:27:58 +0100

    start XFacts with a utility module

---
+++ lib/XFacts/Config.pm
@@ -0,0 +1,40 @@
+package XFacts::Config;
+use 5.012;
+use warnings;
+use Exporter qw(import dbh);
+
+our @EXPORT_OK = qw(config);
+use autodie;
+use JSON::XS qw(decode_json);
+
+use XML::Amazon;
+use DBI;
+
+my $conf = decode_json do {
+    open my $file, '<:encoding(UTF-8)', 'config.json';
+    my $content = do { local $/, <$file> };
+    close $file;
+    $content;
+};
+
+sub config { $conf };
+
+sub dbh {
+    my $dbh = DBI->connect(
+        "dbi:Pg:dbname=$conf->{dbname};host=$conf->{dbhost}",
+        $conf->{dbuser}, $conf->{dbpass},
+        { RaiseError => 1, AutoCommit => 1},
+    );
+    $dbh->{pg_enable_utf8} = 1;
+    $dbh;
+}
+
+sub amazon {
+    XML::Amazon->new(
+        token => $config->{amazon_token},
+        sak   => $config->{amazon_secret_key},
+        local => $config->{amazon_locale} // 'us',
+    );
+}
+
+1;
