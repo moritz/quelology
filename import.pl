24c373613f069a3d59f9c2316716abb8ef17f0ad
Moritz Lenz <moritz@faui2k3.org>
2010-11-23 22:36:13 +0100

    small import script

---
+++ import.pl
@@ -0,0 +1,21 @@
+use warnings;
+use 5.012;
+use lib 'lib';
+use XFacts::Config qw(schema amazon);
+
+my $schema = schema;
+my @asins = qw(0618574948 0618129081 0618574972 0345518705 0345503813
+              006057528X 0060575298 006057528X);
+my @objs = map $schema->resultset('Medium')->from_asin($_), @asins;
+my %rels = (
+    0   => 1,
+    1   => 2,
+    3   => 4,
+    5   => 6,
+    6   => 7,
+);
+while (my ($l, $r) = each %rels) {
+    $objs[$l]->create_related('sequels', {
+            second => $objs[$r]->id,
+    });
+}
