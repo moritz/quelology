9f550d1ee8b64e14e378a69c2a3d4b56e47475dc
Moritz Lenz <moritz@faui2k3.org>
2010-11-24 22:39:58 +0100

    start to switch to nested sets

--- lib/XFacts/Model/Result/Medium.pm
+++ lib/XFacts/Model/Result/Medium.pm
@@ -1,6 +1,7 @@
 package XFacts::Model::Result::Medium;
 use parent qw/DBIx::Class::Core/;
 
+__PACKAGE__->load_components(qw( Tree::NestedSet ));
 __PACKAGE__->table('medium');
 __PACKAGE__->add_columns(qw/
     id
@@ -14,10 +15,19 @@ __PACKAGE__->add_columns(qw/
     medium_image
     large_image
     publish_year
+    root_id
+    l
+    r
+    level
     /);
+
 __PACKAGE__->set_primary_key('id');
 __PACKAGE__->add_unique_constraint(['asin']);
-__PACKAGE__->has_many('sequels', "XFacts::Model::Result::MediumLink", 'first');
-__PACKAGE__->has_many('prequels', "XFacts::Model::Result::MediumLink", 'second');
 
+__PACKAGE__->tree_columns({
+        root_column     => 'root_id',
+        left_column     => 'l',
+        right_column    => 'r',
+        level_column    => 'level',
+});
 1;
--- schema.sql
+++ schema.sql
@@ -1,24 +1,18 @@
 DROP TABLE IF EXISTS medium CASCADE;
 CREATE TABLE medium (
     id SERIAL primary key,
-    asin CHAR(10) unique,
+    asin CHAR(10) UNIQUE,
     ISBN VARCHAR(13),
-    title VARCHAR(255),
+    title VARCHAR(255) NOT NULL,
     made_by VARCHAR(255),
     publisher VARCHAR(255),
     amazon_url VARCHAR(255),
     small_image VARCHAR(255),
     medium_image VARCHAR(255),
     large_image VARCHAR(255),
-    publish_year SMALLINT
+    publish_year SMALLINT,
+    root_id INTEGER REFERENCES medium (id) ON DELETE CASCADE,
+    l INTEGER,
+    r INTEGER,
+    level INTEGER
 );
-
-DROP TABLE IF EXISTS medium_link;
-CREATE TABLE medium_link (
-    id BIGSERIAL primary key,
-    first  INTEGER NOT NULL REFERENCES medium (id) ON DELETE CASCADE,
-    second INTEGER NOT NULL REFERENCES medium (id) ON DELETE CASCADE,
-    link_type VARCHAR(20),
-    UNIQUE(first, second)
-);
-
