68bafaed6221d5b75997d669ed94eedce921c78d
Moritz Lenz <moritz@faui2k3.org>
2010-11-26 19:56:07 +0100

    actually use tree structure

--- import.pl
+++ import.pl
@@ -7,15 +7,18 @@ my $schema = schema;
 my @asins = qw(0618574948 0618129081 0618574972 0345518705 0345503813
               006057528X 0060575298 0060575301);
 my @objs = map $schema->resultset('Medium')->from_asin($_), @asins;
+for (@objs) {
+    say $_->title;
+}
 my %rels = (
-    0   => 1,
-    1   => 2,
-    3   => 4,
-    5   => 6,
-    6   => 7,
+    'The Lord of the Ring' => [0, 1, 2],
+    'Demon Series' => [3, 4],
+    'The Black Magician Trilogy' => [5, 6, 7],
 );
 while (my ($l, $r) = each %rels) {
-    $objs[$l]->create_related('sequels', {
-            second => $objs[$r]->id,
+    say "$l -> $r";
+    my $root = $schema->resultset('Medium')->create({
+            title => $l,
     });
+    $root->attach_rightmost_child($objs[$_]) for @$r;
 }
