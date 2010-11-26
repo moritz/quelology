63cc71e2fc7b725ead749f94522342dfe1a4554c
Moritz Lenz <moritz@faui2k3.org>
2010-11-26 19:49:32 +0100

    fix accidentally duped asin

--- import.pl
+++ import.pl
@@ -5,7 +5,7 @@ use XFacts::Config qw(schema amazon);
 
 my $schema = schema;
 my @asins = qw(0618574948 0618129081 0618574972 0345518705 0345503813
-              006057528X 0060575298 006057528X);
+              006057528X 0060575298 0060575301);
 my @objs = map $schema->resultset('Medium')->from_asin($_), @asins;
 my %rels = (
     0   => 1,
