4211466d201e015e27daa05182c002cbe784d5ab
Moritz Lenz <moritz@faui2k3.org>
2010-12-14 22:28:04 +0100

    move some js

--- web/public/js/custom.js
+++ web/public/js/custom.js
@@ -1,4 +1,23 @@
 $(document).ready(function() {
+    $('.medium').draggable({
+        snap: ".droppable",
+        snapMode: 'inner',
+        cursorAt: { top: 16, left: 16 },
+        helper: function (event) {
+            return $( '<img src="/image/drag.png" >' );
+        }
+    });
+    $('.drop').droppable({
+        drop: function (event, ui) {
+            var target_spec = $(this).attr('id').split('_');
+            var source_id = ui.draggable.attr('id').split('_')[1];
+            $('#source_id').val(ui.draggable.attr('id').split('_')[1]);
+            $('#edit_where').val(target_spec[1]);
+            $('#target_id').val(target_spec[2]);
+            $('#edit_form').submit();
+        }
+    });
+
     $('#header').droppable({
         drop: function(ent, ui) {
             var source_id = ui.draggable.attr('id').split('_')[1];
