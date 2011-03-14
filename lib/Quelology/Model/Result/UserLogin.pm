c9cbf7ed45ec55f67aa764f886d5a7fa99c83829
Moritz Lenz <moritz@faui2k3.org>
2011-03-14 14:12:06 +0100

    authentication tests; pasword update

--- lib/Quelology/Model/Result/UserLogin.pm
+++ lib/Quelology/Model/Result/UserLogin.pm
@@ -1,6 +1,7 @@
 package Quelology::Model::Result::UserLogin;
 use parent qw/DBIx::Class::Core/;
 use utf8;
+use mro 'c3';
 use Crypt::Eksblowfish::Bcrypt qw(bcrypt_hash);
 
 __PACKAGE__->table('user_login');
@@ -25,15 +26,32 @@ __PACKAGE__->add_unique_constraint(['name']);
 sub authenticate {
     my ($self, $password) = @_;
     my $hash = bcrypt_hash({
-            name    => $self->name,
             cost    => $self->cost,
             salt    => $self->salt,
         }, $password);
     if ($hash eq $self->pw_hash) {
         return $self;
     } else {
-        warn "password mismatch for user " . $self->name;
         return;
+    }
+}
+
+sub update {
+    my ($self, $data) = @_;
+
+    if (exists $data->{password}) {
+        my %data_copy = %$data;
+        my $new_pw = delete $data_copy{password};
+        # TODO: check password strength (SEC)
+
+        # TODO: make hack less ugly
+        $data_copy{cost} =  Quelology::Model::ResultSet::UserLogin::COST();
+
+        $data_copy{salt} = join '', map { chr rand 255 } 1..16;
+        $data_copy{pw_hash} = bcrypt_hash(\%data_copy, $new_pw);
+        $self->next::method(\%data_copy);
+    } else {
+        $self->next::method($data);
     }
 }
 
--- t/model.t
+++ t/model.t
@@ -3,6 +3,7 @@ use warnings;
 use 5.012;
 
 use Test::More;
+use Test::Exception;
 use lib 't/lib';
 use lib 'lib';
 use Quelology::Test::Setup;
@@ -15,5 +16,22 @@ ok my $m = $schema->m->by_id(1), 'medium by id';
 like $m->title, qr/Lord of the Rings/, 'can access title';
 like $m->made_by, qr/Tolkien/, 'made_by';
 is length($m->ISBN), 10, 'ISBN is 10 chars long';
+ok my $root = $m->root, 'can get thread root';
+like $root->title, qr/middle earth/i, '...and it is the rigth one';
+
+ok !$schema->resultset('UserLogin')->authenticate('test', 'wrong'),
+    'can NOT authenticate with wrong password';
+ok !$schema->resultset('UserLogin')->authenticate('notthere', 'wrong'),
+    'can NOT with non-existent user';
+
+ok my $t = $schema->resultset('UserLogin')->authenticate('test', 'test123'),
+    'CAN authenticate with correct credentials';
+
+lives_ok {$t->update({password => 'newpw'}) } 'can update password';
+
+ok !$schema->resultset('UserLogin')->authenticate('test', 'test123'),
+    'can NOT authenticate with old credentials';
+ok  $schema->resultset('UserLogin')->authenticate('test', 'newpw'),
+    'CAN authenticate with new credentials';
 
 done_testing;
