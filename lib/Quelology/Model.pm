2a4d334c91ae965ca74f2275987fc90fac98021e
Moritz Lenz <moritz@faui2k3.org>
2011-05-01 14:11:17 +0200

    [model] more searching

--- lib/Quelology/Model.pm
+++ lib/Quelology/Model.pm
@@ -1,5 +1,7 @@
 package Quelology::Model;
 use Carp qw(confess);
+use KinoSearch::Search::IndexSearcher;
+use Quelology::Model::SearchResult;
 
 use parent qw/DBIx::Class::Schema/;
 
@@ -22,6 +24,31 @@ sub login {
         return;
     }
     $user->authenticate($password);
+}
+
+my $ks;
+my $query_parser;
+
+# TODO: make paths more robust
+sub search {
+    my ($self, $terms) = @_;
+    $ks //= KinoSearch::Search::IndexSearcher->new(
+        index => 'data/search-index/common/',
+    );
+    $query_parser //= KinoSearch::Search::QueryParser->new(
+        schema          => $ks->get_schema,
+        default_boolop  => 'AND',
+    );
+    my $hits = $ks->hits(
+        query       => $query_parser->parse($terms),
+        offset      => 0,
+        num_wanted  => 100,
+    );
+    Quelology::Model::SearchResult->new(
+        hits    => $hits,
+        terms   => $terms,
+        schema  => $self,
+    );
 }
 
 __PACKAGE__->load_namespaces();
