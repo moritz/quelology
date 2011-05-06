1c59057cbabce55eccd6e5d1554906ea03f464fe
Moritz Lenz <moritz@faui2k3.org>
2011-05-06 11:12:21 +0200

    [tools] add a tool that imports mostly random raw publications from amazon. Just because we can.

---
+++ tools/raw-publication-import-random-from-amazon.pl
@@ -0,0 +1,25 @@
+use warnings;
+use 5.012;
+use lib 'lib';
+use Quelology::Config qw/schema amazon/;
+use utf8;
+binmode STDOUT, ':encoding(UTF-8)';
+
+my $c = 0;
+my $pubs = schema->p->search(undef, { rows => 100, order_by => \'RANDOM()' });
+while (my $p = $pubs->next) {
+    my $similar = amazon->search(keywords => '"' . $p->title . '"');
+    say $p->title;
+    for (@{ $similar->items }) {
+        next if $p->asin eq $_->asin;
+        next if schema->p->find({ asin => $_->asin });
+        next if schema->rp->find({ asin => $_->asin });
+        next if length($_->title) > 255;
+        my $rp = schema->rp->import_from_amazon_item($_);
+        say '    ', $rp->title, ' ', $rp->publication_date;
+        $c++;
+    }
+    sleep 1;
+}
+
+say "Imported $c raw publications";
