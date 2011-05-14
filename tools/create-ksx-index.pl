9315ad683169d5ee8f41cd70e2554b1bd04b6086
Moritz Lenz <moritz@faui2k3.org>
2011-05-14 23:31:17 +0200

    [tools] split up ksx index into three indexes (author/titles/series)

--- tools/create-ksx-index.pl
+++ tools/create-ksx-index.pl
@@ -26,57 +26,87 @@ sub fulltext {
     );
 }
 
-my $sortable = KinoSearch::Plan::StringType->new( stored => 1, sortable => 1);
 my $string   = KinoSearch::Plan::StringType->new( stored => 1, sortable => 0);
-
-my $ks       = KinoSearch::Plan::Schema->new;
-$ks->spec_field(name => 'id',           type => $string  );
-$ks->spec_field(name => 'type',         type => $sortable);
-$ks->spec_field(name => 'data',         type => fulltext('en'));
-
-my $idx_path = 'data/search-index/common';
-my $indexer  = KinoSearch::Index::Indexer->new(
-    schema  => $ks,
-    index   => $idx_path,
-    create  => 1,
-);
-
 my $qu = schema();
 
-my $c = 0;
-my $authors = $qu->a;
-while (my $a = $authors->next) {
-    no warnings 'uninitialized';
-    $indexer->add_doc({
-        id      => $a->id,
-        type    => 'author',
-        data    => $a->name . '   ' . $a->legal_name,
-    });
-    $c++;
+for my $section (qw(author title series)) {
+    my $ks       = KinoSearch::Plan::Schema->new;
+    $ks->spec_field(name => 'id',           type => $string  );
+    $ks->spec_field(name => 'data',         type => fulltext('en'));
+    $ks->spec_field(name => 'series_id',    type => $string  ) if $section eq 'title';
+
+
+    my $idx_path = "data/search-index/$section";
+    my $indexer  = KinoSearch::Index::Indexer->new(
+        schema  => $ks,
+        index   => $idx_path,
+        create  => 1,
+    );
+
+    my %populate = (
+        author  => \&index_author,
+        title   => \&index_title,
+        series  => \&series_title,
+    );
+
+    my $before = time;
+    my $count = $populate{$section}->($indexer);
+    $indexer->commit();
+    my $time_diff = time - $before;
+    say "Indexed $count items in section $section in $time_diff seconds";
 }
 
+my $prefetch = { prefetch => { author_titles => 'author' } };
 
-my $prefetch = {
-    prefetch => [
-#        'publications',
-        { author_titles => 'author' },
-    ],
-};
-my $titles = $qu->t->root_nodes->search(undef, $prefetch);
-while (my $t = $titles->next) {
-    my $type      = $t->is_single ? 'title' : 'series';
-    my $search_on = $t->is_single ? $t      : $qu->t->search({ root_id => $t->id }, $prefetch);
-    my $authors      = join ', ', map { $_->name  } $search_on->authors;
-    my $titles       = join ', ', map { $_->title } $search_on->all;
-    my $publications = join ', ', map { $_->title, $_->isbn // '' } $search_on->publications;
-    my $combined     = "$titles\n\t$authors\n\t$publications";
+sub index_author {
+    my $indexer = shift;
+    my $c = 0;
+    my $authors = $qu->a;
+    while (my $a = $authors->next) {
+        no warnings 'uninitialized';
+        $indexer->add_doc({
+                id      => $a->id,
+                data    => $a->name . '   ' . $a->legal_name,
+            });
+        $c++;
+    }
+    $c;
+}
+
+sub index_series {
+    my $indexer = shift;
+    my $c = 0;
+    my $titles = $qu->t->threads->search(undef, $prefetch);
+    while (my $t = $titles->next) {
+        my $authors      = join ', ', map { $_->name  } $t->authors;
+        my $titles       = join ', ', map { $_->title } $t, $t->descendants;
+        my $publications = join ', ', map { $_->title, $_->isbn // '' } $t->publications, $t->descendants->publications;
+        my $combined     = "$titles\n\t$authors\n\t$publications";
 #    say $combined;
-    $indexer->add_doc({
-        id      => $t->id,
-        type    => $type,
-        data    => $combined,
-        });
-    $c++;
+        $indexer->add_doc({
+                id          => $t->id,
+                data        => $combined,
+                series_id   => $t->root_id == $t->id ? '' : $t->root_id,
+            });
+        $c++;
+    }
+    $c;
 }
-$indexer->commit;
-say $c, ' titles/series/authors indexed';
+
+sub index_title {
+    my $indexer = shift;
+    my $c = 0;
+    my $titles = $qu->t->singles->search(undef, $prefetch);
+    while (my $t = $titles->next) {
+        my $authors      = join ', ', map { $_->name  } $t->authors;
+        my $publications = join ', ', map { $_->title, $_->isbn // '' } $t->publications, $t->descendants->publications;
+        my $combined     = $t->title . "\n\t$authors\n\t$publications";
+#    say $combined;
+        $indexer->add_doc({
+                id          => $t->id,
+                data        => $combined,
+            });
+        $c++;
+    }
+    $c;
+}
