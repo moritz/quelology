9f71c1d962dd054dd443fadf9be029900930bca6
Moritz Lenz <moritz@faui2k3.org>
2011-05-18 22:21:17 +0200

    make xisbn.pl loop over arguments

--- tools/xisbn.pl
+++ tools/xisbn.pl
@@ -7,83 +7,81 @@ binmode STDOUT, ':encoding(UTF-8)';
 
 # see http://xisbn.worldcat.org/xisbnadmin/doc/api.htm#response
 
-my $isbn;
 
 use Mojo::UserAgent;
 use Mojo::DOM;
 use lib 'lib';
 use Quelology::Config qw/schema amazon/;
 
-if (@ARGV) {
-    $isbn = shift @ARGV;
-} else {
-    $isbn = schema->p->search(undef, { rows => 1, order_by => \'RANDOM()' })->first->isbn;
+unless (@ARGV) {
+    @ARGV = schema->p->search(undef, { rows => 1, order_by => \'RANDOM()' })->first->isbn;
 }
-
-my $root_title = schema->p->find({asin => $isbn })->title_obj;
-say ' ' x 2, $root_title->id, ' ', $root_title->title;
-say $isbn;
-use Data::Dumper;
-my $data = from_isbn($isbn);
-while (my ($new_isbn, $attrs) = each %$data) {
-    say $new_isbn;
-    unless (defined $attrs->{lang}) {
-        say '    language unknown';
-        next;
-    };
-    say ' ' x 4, $attrs->{lang};
-    if (my $np = schema->p->find({asin => $new_isbn})) {
-        say '    publication known';
-        unless ($np->lang) {
-            say '    updating language';
-            $np->update({ lang => $attrs->{lang} });
-            $np->create_related('attributions', {
-                url     => $attrs->{url},
-                name    => 'worldcat',
-            });
-        }
-        next;
-    }
-
-    schema->txn_do(sub {
-        my $rp = eval { schema->rp->import_by_asin($new_isbn) };
-        return unless $rp;
-        eval {
-            $rp->update({ lang => $attrs->{lang}}) unless $rp->lang eq $attrs->{lang};
-            my $title = $root_title->lang eq $attrs->{lang}
-                        ? $root_title
-                        : $root_title->translations->search({ lang => $attrs->{lang} })->first;
-            if (defined $title) {
-                say "    adding to existing title";
-                $rp->cook($title);
-            } else {
-                my $t = $rp->title;
-                $t =~ s/\s*\((German|Spanish|French|Chinese|Russian) Edition\)$//;
-                printf "    creating new title '%s' as %s translation of '%s'\n",
-                        $t, $rp->lang, $root_title->title;
-                my $title = $root_title->create_related('aliases', {
-                        title   => $t,
-                        lang    => $rp->lang
+for my $isbn (@ARGV) {
+    my $root_title = schema->p->find({asin => $isbn })->title_obj;
+    say ' ' x 2, $root_title->id, ' ', $root_title->title;
+    say $isbn;
+    use Data::Dumper;
+    my $data = from_isbn($isbn);
+    while (my ($new_isbn, $attrs) = each %$data) {
+        say $new_isbn;
+        unless (defined $attrs->{lang}) {
+            say '    language unknown';
+            next;
+        };
+        say ' ' x 4, $attrs->{lang};
+        if (my $np = schema->p->find({asin => $new_isbn})) {
+            say '    publication known';
+            unless ($np->lang) {
+                say '    updating language';
+                $np->update({ lang => $attrs->{lang} });
+                $np->create_related('attributions', {
+                    url     => $attrs->{url},
+                    name    => 'worldcat',
                 });
-                for ($root_title->author_titles) {
-                    $title->create_related('author_titles',
+            }
+            next;
+        }
+
+        schema->txn_do(sub {
+            my $rp = eval { schema->rp->import_by_asin($new_isbn) };
+            return unless $rp;
+            eval {
+                $rp->update({ lang => $attrs->{lang}}) unless $rp->lang eq $attrs->{lang};
+                my $title = $root_title->lang eq $attrs->{lang}
+                            ? $root_title
+                            : $root_title->translations->search({ lang => $attrs->{lang} })->first;
+                if (defined $title) {
+                    say "    adding to existing title";
+                    $rp->cook($title);
+                } else {
+                    my $t = $rp->title;
+                    $t =~ s/\s*\((German|Spanish|French|Chinese|Russian) Edition\)$//;
+                    printf "    creating new title '%s' as %s translation of '%s'\n",
+                            $t, $rp->lang, $root_title->title;
+                    my $title = $root_title->create_related('aliases', {
+                            title   => $t,
+                            lang    => $rp->lang
+                    });
+                    for ($root_title->author_titles) {
+                        $title->create_related('author_titles',
+                            {
+                                author_id => $_->author_id,
+                            },
+                        );
+
+                    }
+                    $rp->cook($title);
+                    $title->create_related('attributions',
                         {
-                            author_id => $_->author_id,
+                            url     => $attrs->{url},
+                            name    => 'worldcat',
                         },
                     );
-
                 }
-                $rp->cook($title);
-                $title->create_related('attributions',
-                    {
-                        url     => $attrs->{url},
-                        name    => 'worldcat',
-                    },
-                );
-            }
-        };
-        warn $@ if $@;
-    });
-}
+            };
+            warn $@ if $@;
+        });
+    }
 
-say ' ' x 2, $root_title->id, ' ', $root_title->title;
+    say ' ' x 2, $root_title->id, ' ', $root_title->title;
+}
