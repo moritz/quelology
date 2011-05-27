37809c24de17c2dae51e4a12744154405400e905
Moritz Lenz <moritz@faui2k3.org>
2011-05-27 10:34:55 +0200

    [tools/xisbn] push stuff into a sub; no functional changes

--- tools/xisbn.pl
+++ tools/xisbn.pl
@@ -47,58 +47,64 @@ for my $isbn (@ARGV) {
             }
             next;
         }
+        import_related_isbn($root_title, $new_isbn, $attrs);
 
-        schema->txn_do(sub {
-            my $rp = eval { schema->rp->import_by_asin($new_isbn) };
-            print $@ if $@;
-            unless ($rp) {
-                $rp = schema->rp->import_from_xisbn_attrs($attrs);
-                say "    using worldcat data for raw publication (" . $rp->id . ")";
-            }
-            # there some weird "publications" which are actually audio players
-            # with a certain audio title preloaded. Those don't add any value,
-            # so filter 'em out
-            if ($rp->title =~ /With Earbuds/) {
-                $rp->delete;
-                return;
-            }
-            eval {
-                $rp->update({ lang => $attrs->{lang}}) unless $rp->lang eq $attrs->{lang};
-                my $title = $root_title->lang eq $attrs->{lang}
-                            ? $root_title
-                            : $root_title->translations->search({ lang => $attrs->{lang} })->first;
-                if (defined $title) {
-                    say "    adding to existing title";
-                    $rp->cook($title);
-                } else {
-                    my $t = $rp->title;
-                    $t =~ s/\s*\((German|Spanish|French|Chinese|Russian) Edition\)$//;
-                    printf "    creating new title '%s' as %s translation of '%s'\n",
-                            $t, $rp->lang, $root_title->title;
-                    my $title = $root_title->create_related('aliases', {
-                            title   => $t,
-                            lang    => $rp->lang
-                    });
-                    for ($root_title->author_titles) {
-                        $title->create_related('author_titles',
-                            {
-                                author_id => $_->author_id,
-                            },
-                        );
-
-                    }
-                    $rp->cook($title);
-                    $title->create_related('attributions',
-                        {
-                            url     => $attrs->{url},
-                            name    => 'worldcat',
-                        },
-                    );
-                }
-            };
-            warn $@ if $@;
-        });
     }
 
     say ' ' x 2, $root_title->id, ' ', $root_title->title;
+}
+
+sub import_related_isbn {
+    my ($root_title, $new_isbn, $attrs) = @_;
+    schema->txn_do(sub {
+        my $rp = eval { schema->rp->import_by_asin($new_isbn) };
+        print $@ if $@;
+        unless ($rp) {
+            $rp = schema->rp->import_from_xisbn_attrs($attrs);
+            say "    using worldcat data for raw publication (" . $rp->id . ")";
+        }
+        # there some weird "publications" which are actually audio players
+        # with a certain audio title preloaded. Those don't add any value,
+        # so filter 'em out
+        if ($rp->title =~ /With Earbuds/) {
+            $rp->delete;
+            return;
+        }
+        eval {
+            $rp->update({ lang => $attrs->{lang}}) unless $rp->lang eq $attrs->{lang};
+            my $title = $root_title->lang eq $attrs->{lang}
+                        ? $root_title
+                        : $root_title->translations->search({ lang => $attrs->{lang} })->first;
+            if (defined $title) {
+                say "    adding to existing title";
+                $rp->cook($title);
+            } else {
+                my $t = $rp->title;
+                $t =~ s/\s*\((German|Spanish|French|Chinese|Russian) Edition\)$//;
+                printf "    creating new title '%s' as %s translation of '%s'\n",
+                        $t, $rp->lang, $root_title->title;
+                my $title = $root_title->create_related('aliases', {
+                        title   => $t,
+                        lang    => $rp->lang
+                });
+                for ($root_title->author_titles) {
+                    $title->create_related('author_titles',
+                        {
+                            author_id => $_->author_id,
+                        },
+                    );
+
+                }
+                $rp->cook($title);
+                $title->create_related('attributions',
+                    {
+                        url     => $attrs->{url},
+                        name    => 'worldcat',
+                    },
+                );
+            }
+        };
+        warn $@ if $@;
+    });
+
 }
