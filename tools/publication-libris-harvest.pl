43565a02b4495e04df9731fe1cf1c85727c6f1d9
Moritz Lenz <moritz@faui2k3.org>
2011-06-18 16:16:38 +0200

    document optional depencies. Add a tool for importing data from libris.kb.se

---
+++ tools/publication-libris-harvest.pl
@@ -0,0 +1,29 @@
+use 5.010;
+use strict;
+use warnings;
+use lib 'lib';
+use Quelology::Config qw/schema/;
+use WebService::Libris;
+binmode STDOUT, ':encoding(UTF-8)';
+
+my $s = schema;
+my $rs = $s->p->search({ isbn => { '<>' => undef }, lang => ['sv', 'no', 'nn', 'nb', 'dk', undef]});
+
+while (my $p = $rs->next) {
+    say $p->title;
+    my $l = WebService::Libris->search_for_isbn($p->isbn);
+    if ($l) {
+        say '   libris id: ', $l->id;
+        eval {
+            $p->update({ libris_id => $l->id });
+        };
+        warn $@ if $@;
+        eval {
+            if (!$p->lang && $l->language) {
+                say '    updating language to ', $l->language;
+                $p->update({ lang => $l->language });
+            }
+        };
+        warn $@ if $@;
+    }
+}
