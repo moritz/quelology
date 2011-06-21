b2d83e48416f9adbb6637577cd099edfbd70b298
Moritz Lenz <moritz@faui2k3.org>
2011-06-21 17:27:18 +0200

    [t/lib] diagnostics while initializing test db

--- t/lib/Quelology/Test/Setup.pm
+++ t/lib/Quelology/Test/Setup.pm
@@ -1,3 +1,4 @@
+use 5.010;
 package Quelology::Test::Setup;
 use strict;
 use warnings;
@@ -15,7 +16,9 @@ sub init_db {
     # TODO: read that info from the config file
     my ($user, $dbname, $host) = map config($_), qw/dbuser dbname dbhost/;
 
+    say '# initilizing test db... this might take a while';
     system("psql $dbname -h $host -U $user < t/db-snapshot.sql > /dev/null 2>&1");
+    say '# ... done initilizing test db.';
 }
 
 1;
